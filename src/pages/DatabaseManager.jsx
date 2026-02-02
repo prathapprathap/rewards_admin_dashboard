1586852fa2f252f552e73a3335421c4c31034746
prathapshanmugam3 <prathapshanmugam5@gmail.com>
2026-02-02 20:46:58 +0530

    table changed

--- src/pages/DatabaseManager.jsx
+++ src/pages/DatabaseManager.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+\import { useEffect, useState } from 'react';
 import { FaEdit, FaPlus, FaTrash } from 'react-icons/fa';
 import { createRecord, deleteRecord, getTableData, getTables, getTableStructure, updateRecord } from '../api';
 
